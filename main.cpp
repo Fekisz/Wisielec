e66fdc0d76ece9430f2c936a8cffa959bb42c90c
Fekisz <119873700+Fekisz@users.noreply.github.com>
2022-12-19 19:18:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <conio.h>
 using namespace std;
 int opcja,g=0,dlu,d;
-string x1="spadochron",x2="auto",x3="ahmed",x4="pies",x5="kot",x6="golf",x7="zegar",x8="poduszka",x9="tratwa",x10="kura",x11="wrona",z;
+string z, x[]={"spadochron","auto","ahmed","pies","kot","golf","zegar","poduszka","tratwa","kura","wrona"};
 char y,h,c[20];
 
 int main()
@@ -18,48 +18,8 @@ Play:
     cout << "wisielec" << endl;
     srand(time(NULL));
     opcja=rand() % 11;
-    cout<<++opcja<<endl ;
-    switch(opcja)
-    {
-    case 1:
-        z=x1;
-        break;
-    case 2:
-        z=x2;
-        break;
-    case 3:
-        z=x3;
-        break;
-    case 4:
-        z=x4;
-        break;
-    case 5:
-        z=x5;
-        break;
-    case 6:
-        z=x6;
-        break;
-    case 7:
-        z=x7;
-        break;
-    case 8:
-        z=x8;
-        break;
-    case 9:
-        z=x9;
-        break;
-    case 10:
-        z=x10;
-        break;
-    case 11:
-        z=x11;
-        break;
-
-    default:
-        cout<<"coś się zepsuł"<<endl;
-        exit(0);
-        break;
-    }
+    //cout<<++opcja<<endl ;
+    z=x[opcja-1];
 
     dlu = z.length();
     cout<<dlu<<endl;
